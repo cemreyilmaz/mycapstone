89f8c283f96df63a8c13b374c1d13180118da842
cemreyilmaz <cemreyilmaz@yahoo.com>
2021-11-08 22:27:49 +0100

    test the package

--- tests/tests.R
+++ tests/tests.R
@@ -1,12 +1,13 @@
-library(tester)
-context("mycapstone")
+library(mycapstone)
+testthat::context("mycapstone")
 
 data <- utils::read.delim("earthquakes.tsv")
 
 testthat::test_that("eq_clean_data returns a data.frame", {
-  testthat::expect_true(is.data.frame(eq_clean_data(data)))
+  testthat::expect_true(is.data.frame(mycapstone::eq_clean_data(data)))
 })
 
 testthat::test_that("eq_location_clean returns a data.frame", {
-  testthat::expect_true(is.data.frame(eq_location_clean(data)))
+  testthat::expect_true(is.data.frame(mycapstone::eq_location_clean(data)))
 })
+
